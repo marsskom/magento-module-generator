15aaf041bab4e5ffd9fe31a4ba21b8b075ea070e
Andrii Prakapas <marsskom@gmail.com>
2022-04-28 08:22:27 +0300

    feat(lambda): lambda #59.
    + Delete all codebase.
    
    + Keep stubs, global factory and console commands samples.

--- Console/Command/GeneratorCommand.php
+++ Console/Command/GeneratorCommand.php
@@ -6,98 +6,32 @@ namespace Marsskom\Generator\Console\Command;
 
 use Magento\Framework\Console\Cli;
 use Magento\Framework\Exception\LocalizedException;
-use Marsskom\Generator\Api\Data\ComponentManagerInterface;
-use Marsskom\Generator\Model\Scope\ScopeBuilder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class GeneratorCommand extends Command
 {
-    protected ComponentManagerInterface $componentManager;
-
-    protected ScopeBuilder $scopeBuilder;
-
-    /**
-     * Command constructor.
-     *
-     * @param ComponentManagerInterface $componentManager
-     * @param ScopeBuilder              $scopeBuilder
-     * @param string|null               $name
-     */
-    public function __construct(
-        ComponentManagerInterface $componentManager,
-        ScopeBuilder $scopeBuilder,
-        string $name = null
-    ) {
-        $this->componentManager = $componentManager;
-        $this->scopeBuilder = $scopeBuilder;
-
-        parent::__construct($name);
-    }
-
     /**
      * @inheritdoc
      */
     protected function configure(): void
     {
-        $this->setDefinition($this->componentManager->inputDefinition());
+        $this->setDefinition();
 
         parent::configure();
     }
-
-    /**
-     * Returns event name.
-     *
-     * @return string
-     */
-    abstract protected function getEventName(): string;
 
     /**
      * @inheritdoc
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        if (!$this->validate($input, $output)) {
-            return Cli::RETURN_FAILURE;
-        }
-
         try {
-            $this->componentManager->sequence()->execute(
-                $this->scopeBuilder->create($input, $output)
-            );
         } catch (LocalizedException $exception) {
-            $output->writeln($exception->getMessage());
-
-            $output->writeln($exception->getTraceAsString());
-
             return Cli::RETURN_FAILURE;
         }
 
         return Cli::RETURN_SUCCESS;
-    }
-
-    /**
-     * Validates input params.
-     *
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return bool
-     */
-    protected function validate(InputInterface $input, OutputInterface $output): bool
-    {
-        $validateResult = $this->componentManager
-            ->validationObserver()
-            ->notify(
-                $this->getEventName(),
-                $input->getOptions()
-            );
-
-        if (!$validateResult->isValid()) {
-            $output->writeln($validateResult->getMessage());
-        }
-
-        return $validateResult->isValid();
     }
 }
--- Console/Command/ModuleCommand.php
+++ Console/Command/ModuleCommand.php
@@ -4,28 +4,8 @@ declare(strict_types = 1);
 
 namespace Marsskom\Generator\Console\Command;
 
-use Marsskom\Generator\Model\Manager\ModuleManager;
-use Marsskom\Generator\Model\Scope\ScopeBuilder;
-
 class ModuleCommand extends GeneratorCommand
 {
-    public const EVENT_NAME = 'console_command_module-command';
-
-    /**
-     * @inheritdoc
-     *
-     * @param ModuleManager $moduleManager
-     *
-     * phpcs:disable Generic.CodeAnalysis.UselessOverridingMethod
-     */
-    public function __construct(
-        ModuleManager $moduleManager,
-        ScopeBuilder $scopeBuilder,
-        string $name = null
-    ) {
-        parent::__construct($moduleManager, $scopeBuilder, $name);
-    }
-
     /**
      * @inheritdoc
      */
@@ -35,13 +15,5 @@ class ModuleCommand extends GeneratorCommand
         $this->setDescription("Generates module.");
 
         parent::configure();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function getEventName(): string
-    {
-        return self::EVENT_NAME;
     }
 }
