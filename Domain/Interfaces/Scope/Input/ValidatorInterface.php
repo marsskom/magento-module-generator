02129da485f11f7973b4c55a385699c8b300dab6
Andrii Prakapas <marsskom@gmail.com>
2022-04-28 16:53:37 +0300

    feat(lambda): add validator interface #59

---
+++ Domain/Interfaces/Scope/Input/ValidatorInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Marsskom\Generator\Domain\Interfaces\Scope\Input;
+
+use Marsskom\Generator\Domain\Interfaces\Callables\CallableInterface;
+
+interface ValidatorInterface extends CallableInterface
+{
+}
